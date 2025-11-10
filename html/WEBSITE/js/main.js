319a5a6679449ade1c668ccc4f1438384fd63e52
SkrinVal <skrinval@outlook.de>
2025-11-10 17:57:17 +0100

    Add files via upload

---
+++ html/WEBSITE/js/main.js
@@ -0,0 +1,13 @@
+    function goToBooking() {
+      window.location.href = "booking.html";
+    }
+
+    function goToExamples() {
+      window.location.href = "example.html";
+    }    
+
+    function goToStart() {
+      window.location.href = "index.html";
+    }    
+
+
